9455b781a24155298388374674ea6c0abc8a7e1a
Serena Guo <syg9345@gmail.com>
2022-07-15 15:24:35 -0400

    set up firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase@app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDR0xdQUHnIX3b7NehVnF5oa8zUVEWSaoA",
+    authDomain: "vue-live-chat-53ee8.firebaseapp.com",
+    projectId: "vue-live-chat-53ee8",
+    storageBucket: "vue-live-chat-53ee8.appspot.com",
+    messagingSenderId: "63048641047",
+    appId: "1:63048641047:web:6816f2ba7c73f612e40a52"
+};
+
+firebase.initializeApp(firebaseConfig)
+const projectFirestore = firebase.firestore()
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export { projectFirestore, timestamp }
